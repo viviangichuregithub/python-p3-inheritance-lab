1a1e5ed274984d06fcb6726be7ddf108bbdd3972
Vivian Gichure <viviangichure@gmail.com>
2025-08-13 11:19:49 +0300

    completed lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []  # Start with an empty list
+
+    def learn(self, knowledge_string):
+        # Add the given string to the student's knowledge list
+        self.knowledge.append(knowledge_string)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
 from user import User
-
 import random
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge  # Initialize with the given list
 
     def teach(self):
-        pass
+        # Return a random element from the knowledge list
+        return random.choice(self.knowledge)
